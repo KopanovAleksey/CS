d72dcbc2f248112f1d890f21d54e87317b33ebb5
Alexey <asaleseksesey12345@gmail.com>
2023-02-23 20:56:35 +0300

    First task is done

---
+++ seminar1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число");
+string userAnswer = Console.ReadLine();
+int number = Convert.ToInt32(userAnswer); // intnumber = Convert.ToInt32(Console.ReadLine())
+int result = number * number;
+Console.WriteLine("Квадрат вашего числа равен " + result);
