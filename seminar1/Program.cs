71e5fc59ff933f5eb0aae011a9abf8325d648bb1
Alexey <asaleseksesey12345@gmail.com>
2023-02-23 21:09:04 +0300

    second task

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -1,5 +1,34 @@
-﻿Console.WriteLine("Введите число");
-string userAnswer = Console.ReadLine();
-int number = Convert.ToInt32(userAnswer); // intnumber = Convert.ToInt32(Console.ReadLine())
-int result = number * number;
-Console.WriteLine("Квадрат вашего числа равен " + result);
+﻿// Console.WriteLine("Введите число");
+// string userAnswer = Console.ReadLine();
+// int number = Convert.ToInt32(userAnswer); // int number = Convert.ToInt32(Console.ReadLine())
+// int result = number * number;
+// Console.WriteLine("Квадрат вашего числа равен " + result);
+
+// Console.WriteLine("Enter two numbers");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if (number1 > number2)
+// {
+//     if (number1 / number2 == number2)
+//     {
+//         Console.WriteLine("Число являектся квадратом");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Число не являектся квадратом");
+//     }
+
+// }
+// else
+// {
+//     if (number2 / number1 == number1)
+//     {
+//         Console.WriteLine("Число являектся квадратом");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Число не являектся квадратом");
+//     }
+
+// }
