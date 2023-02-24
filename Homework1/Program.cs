ece1f960efaf589c828322049bbffa3d415aeb8e
Alexey <asaleseksesey12345@gmail.com>
2023-02-24 12:58:27 +0300

    task 8 solved

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -37,6 +37,7 @@ Console.WriteLine("Максимальное число " + max);
 
 //Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
 
+/*
 Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number % 2 == 0)
@@ -46,4 +47,21 @@ if (number % 2 == 0)
 else
 {
     Console.WriteLine("Ваше число нечетное");
+}
+*/
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+int N;
+do
+{
+    Console.WriteLine("Введите положительное число");
+    N = Convert.ToInt32(Console.ReadLine());
+} while (N <= 0);
+
+for (int i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
 }
