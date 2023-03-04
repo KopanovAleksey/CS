46ba444d0e0531f61ee5a56b49322913525766a6
Alexey <asaleseksesey12345@gmail.com>
2023-03-04 16:42:41 +0300

    Homework3 Task 21

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -24,3 +24,21 @@ else
     Console.WriteLine("Ваше число не палиндром");
 }
 */
+//Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координаты 2ух точек в 3D пространстве ");
+Console.WriteLine("Введите координату x1 точки");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y1 точки");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату z1 точки");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату x2 точки");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y2 точки");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату z2 точки");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double lenght = Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)+Math.Pow(z1 - z2, 2)), 1);
+Console.WriteLine("Длина отрезка равна = " + lenght);
