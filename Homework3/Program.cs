127f8e72f0ad51a00e3a65154c47841b1207a5fa
Alexey <asaleseksesey12345@gmail.com>
2023-03-04 16:46:11 +0300

    Homework3 Task 23

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -25,7 +25,7 @@ else
 }
 */
 //Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-
+/*
 Console.WriteLine("Введите координаты 2ух точек в 3D пространстве ");
 Console.WriteLine("Введите координату x1 точки");
 double x1 = Convert.ToDouble(Console.ReadLine());
@@ -42,3 +42,16 @@ double z2 = Convert.ToDouble(Console.ReadLine());
 
 double lenght = Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)+Math.Pow(z1 - z2, 2)), 1);
 Console.WriteLine("Длина отрезка равна = " + lenght);
+*/
+//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int n = 0;
+do
+{
+    Console.WriteLine("Введите число больше 0");
+    n = Convert.ToInt32(Console.ReadLine());
+} while (n < 1);
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"{i}^2 = {Math.Pow(i, 3)}");
+}
