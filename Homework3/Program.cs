42ef9f7194b1fdb2c7a4a9c55109ea2a5491d63e
Alexey <asaleseksesey12345@gmail.com>
2023-03-04 16:35:43 +0300

    Homework3 Task 19

---
+++ Homework3/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+/*
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int temp = n;
+do
+{
+    temp /= 10;
+    i++;
+} while (temp != 0);
+int j = i;
+while (i > 0)
+{
+    temp = temp + n % (int)Math.Pow(10, i) / (int)Math.Pow(10, i - 1) * (int)Math.Pow(10, j - i);
+    i--;
+}
+if (n == temp)
+{
+    Console.WriteLine("Ваше число палиндром");
+}
+else
+{
+    Console.WriteLine("Ваше число не палиндром");
+}
+*/
